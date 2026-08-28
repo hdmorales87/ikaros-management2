5ecf4ed86a0188d93ae88746b0516e98ab46cb6c
hdmorales87 <hdmorales87@gmail.com>
2026-08-28 18:10:42 -0500

    Update datagrid.php

--- backend/config/datagrid.php
+++ backend/config/datagrid.php
@@ -2,7 +2,7 @@
 
 return [
     'tables' => [
-        'users', 'roles', 'permisos', 'terceros', 'terceros_contratos', 'terceros_contratos_estados', 'terceros_contratos_notificaciones', 'terceros_contratos_pagos', 'terceros_contratos_planes_pagos', 'imap', 'imap_reglas', 'areas_servicio',
+        'users', 'roles', 'permisos', 'terceros', 'terceros_contratos', 'terceros_contratos_adjuntos', 'terceros_contratos_estados', 'terceros_contratos_notificaciones', 'terceros_contratos_pagos', 'terceros_contratos_pagos_adjuntos', 'terceros_contratos_planes_pagos', 'imap', 'imap_reglas', 'areas_servicio',
         'areas_servicio_categorias', 'areas_servicio_subcategorias',
         'categorias', 'subcategorias', 'incidencias', 'problemas', 'servicios',
         'proyectos_actividades', 'proyectos_actividades_responsables', 'proyectos_subactividades', 'proyectos_riesgos', 'proyectos_riesgos_probabilidad', 'proyectos_riesgos_impacto',
